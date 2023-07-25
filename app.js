ea19873412b98e46a5d7a2acf526c74ec77f7bee
Lucasfullstack <cai_luk@yahoo.com>
2023-07-25 19:54:49 -0300

    subir

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const app = express()
+const path = require("path")
+
+app.use(express.static("public"))
+app.listen(3000,()=>{
+    console.log("servidor corriendo")
+    
+app.get("/", function(req,res){
+res.sendfile(path.join(__dirname,"/views/home.html"))
+    })
+})
+app.get("/register", function(req,res){
+    res.sendfile(path.join(__dirname,"/views/register.html"))
+})
+app.get("/login", function(req,res){
+    res.sendfile(path.join(__dirname,"/views/login.html"))
+})
---
+++ views/readme.md
@@ -0,0 +1,8 @@
+<i class="fa fa-search"></i>
+<i class="far fa-id-card"></i>
+<i class="fas fa-sign-in-alt"></i>
+<i class="fas fa-shopping-basket"></i>
+<i class="fa fa-bars burger"></i>
+<i class="fas fa-credit-card"></i>
+<i class="fas fa-gift"></i>
+<i class="fas fa-user-shield"></i>
